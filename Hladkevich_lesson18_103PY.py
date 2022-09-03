d940feca47b13a7ef0a2f6d88a7aacaea6a0a98f
g14s3r <g14s3r@gmail.com>
2022-09-03 23:52:56 +0300

    Added commentaries

--- Hladkevich_lesson18_103PY.py
+++ Hladkevich_lesson18_103PY.py
@@ -1,4 +1,4 @@
-class Tomato:
+class Tomato:  # Объявление класса
     states = {
         1: 'First true leaves',
         2: 'Growth',
@@ -8,11 +8,11 @@ class Tomato:
         6: 'Fetus maturation'
     }
 
-    def __init__(self, index):
+    def __init__(self, index):  # Инициализатор класса
         self._index = index
         self._state = 1
 
-    def grow(self):
+    def grow(self):  # Метод, переводящий на след. стадию созревания
         self._state += 1
 
     def cur_state(self):
@@ -22,25 +22,23 @@ class Tomato:
     def cur_name(self):
         return self.states.get(self.current)
 
-    def is_ripe(self):
+    def is_ripe(self):  # Метод, проверяющий созревание
         if self._state == 6:
-            # print('The fetus is ripe')
             return True
         else:
-            # print(f'The fetus isn\'t yet ripe. Because state is yet {self._state}')
             return False
 
 
-class TomatoBush:
-    def __init__(self, amount):
+class TomatoBush:  # Объявление класса
+    def __init__(self, amount):  # Инициализатор класса
         self.amount = amount
         self.tomatoes = [Tomato(index=i) for i in range(1, self.amount + 1)]
 
-    def grow_all(self):
+    def grow_all(self):  # Метод, преводящий объекты из списка на след. стадию
         for _ in self.tomatoes:
             _.grow()
 
-    def all_are_rip(self):
+    def all_are_rip(self):  # Метод, проверяющий созревание всех томатов
         count = 0
         for _ in self.tomatoes:
             if _.is_ripe():
@@ -62,30 +60,31 @@ class TomatoBush:
                 self.name = _.cur_name()
         return self.name
 
-    def dive_away_all(self):
+    def dive_away_all(self):  # Метод для очистки списка
         if self.all_are_rip():
             self.tomatoes.clear()
         return self.tomatoes
 
 
-class Gardener:
-    def __init__(self, name, plant):
+class Gardener:  # Объявление класса
+    def __init__(self, name, plant):  # Инициализатор класса
         self.name = name
         self._plant = plant
 
-    def work(self):
+    def work(self):  # Метод, заставляющий работника работать
         self._plant.grow_all()
 
-    def harvest(self):
+    def harvest(self):  # Метод, проверяющий созревание урожая
         self.foo = self._plant.number_state()
         self.bar = self._plant.name_state()
         if self._plant.all_are_rip():
             print('The harvest is ripe')
         else:
-            print(f'The harvest isn\'t yet ripe. The process is in the state {self.foo} - "{self.bar}"')
+            print(f'The harvest isn\'t yet ripe. The process is in the '
+                  f'state {self.foo} - "{self.bar}"')
 
     @staticmethod
-    def knowledge_base():
+    def knowledge_base():  # Статический метод. Справка по садоводству
         print('Gardening is the practice of growing and cultivating plants as '
               'part of horticulture. In gardens, ornamental plants are often '
               'grown for their flowers, foliage, or overall appearance; useful '
@@ -94,6 +93,7 @@ class Gardener:
               'medicinal or cosmetic use.')
 
 
+# Тесты
 Gardener.knowledge_base()
 bush = TomatoBush(3)
 jim = Gardener('Jim', bush)
