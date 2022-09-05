dfba04e3289c1cdc7c062ce0091bf6a696ea77c8
g14s3r <g14s3r@gmail.com>
2022-09-05 20:27:59 +0300

    Added comments

--- Hladkevich_lesson19_103PY.py
+++ Hladkevich_lesson19_103PY.py
@@ -1,15 +1,15 @@
-def debug(func):
-    def wrapper(a, b):
+def debug(func):  # создание декоратора
+    def wrapper(a, b):  # создание функции-обёртки
         print(f'Launch a funciton {func.__name__}, which has arguments: a = {a}'
-              f' and b = {b}')
-        print(f'Function result is {func(a, b)}')
+              f' and b = {b}')  # вывод названия функции и её аргументов
+        print(f'Function result is {func(a, b)}')  # вывод результата функции
 
     return wrapper
 
 
 @debug
-def func1(a, b):
+def func1(a, b):  # оборачиваемая функции
     return a ** b
 
 
-func1(4, 2)
+func1(4, 2)  # вызов функции
