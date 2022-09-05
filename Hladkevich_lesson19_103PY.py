edd7d9d4f2982c649888a5885664fc803f752996
g14s3r <g14s3r@gmail.com>
2022-09-05 20:31:24 +0300

    Added a task from classwork

--- Hladkevich_lesson19_103PY.py
+++ Hladkevich_lesson19_103PY.py
@@ -13,3 +13,33 @@ def func1(a, b):  # оборачиваемая функции
 
 
 func1(4, 2)  # вызов функции
+
+# ============================================================================
+
+lst = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+def decor(func):
+    def wrapper(foo):
+        count = 0
+        for i in foo:
+            for j in i:
+                count += 1
+        res = count - len(func(foo))
+        print(func(foo))
+        print(f'Amount of numbers that aren\'t multiples of 3 equal {res}')
+
+    return wrapper
+
+
+@decor
+def func2(foo):
+    bar = []
+    for i in foo:
+        for j in i:
+            if j % 3 == 0:
+                bar.append(j)
+    return bar
+
+
+func2(lst)
